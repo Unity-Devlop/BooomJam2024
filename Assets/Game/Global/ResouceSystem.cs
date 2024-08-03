3e140b72485872fd0301fd802a9520332c2b469d
NicoIer <91652330+NicoIer@users.noreply.github.com>
2024-08-03 22:13:17 +0800

    feat:删除异常的Spine生成文件

---
+++ Assets/Game/Global/ResouceSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityToolkit;
+
+namespace Game
+{
+    public class ResouceSystem : ISystem, IOnInit
+    {
+        public void Dispose()
+        {
+        }
+
+        public void LoadPokemonSpine()
+        {
+            
+        }
+
+        public void OnInit()
+        {
+        }
+    }
+}
