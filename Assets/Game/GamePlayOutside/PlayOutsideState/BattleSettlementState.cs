ed270a81f27caee9aab72c169b90417da9672f07
NicoIer <91652330+NicoIer@users.noreply.github.com>
2024-08-04 18:57:34 +0800

    fix:结算流程位置异常

--- Assets/Game/GamePlayOutside/PlayOutsideState/BattleSettlementState.cs
+++ Assets/Game/GamePlayOutside/PlayOutsideState/BattleSettlementState.cs
@@ -6,12 +6,13 @@ namespace Game
     {
         public void OnInit(GamePlayOutsideMgr owner, IStateMachine<GamePlayOutsideMgr> stateMachine)
         {
-            BattleSettlementData data = stateMachine.GetParam<BattleSettlementData>(Consts.BattleSettlementData);
-            // TODO Do something with data
         }
 
         public void OnEnter(GamePlayOutsideMgr owner, IStateMachine<GamePlayOutsideMgr> stateMachine)
         {
+            
+            BattleSettlementData data = stateMachine.GetParam<BattleSettlementData>(Consts.BattleSettlementData);
+            // TODO Do something with data
         }
 
         public void OnUpdate(GamePlayOutsideMgr owner, IStateMachine<GamePlayOutsideMgr> stateMachine)
