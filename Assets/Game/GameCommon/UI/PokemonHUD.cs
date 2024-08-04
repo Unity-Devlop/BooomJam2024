819d0a2c4666bd8d9ef60ac22a1c763b00a694d6
NicoIer <91652330+NicoIer@users.noreply.github.com>
2024-08-04 21:05:13 +0800

    feat:修改FirstChoosePanel

--- Assets/Game/GameCommon/UI/PokemonHUD.cs
+++ Assets/Game/GameCommon/UI/PokemonHUD.cs
@@ -22,7 +22,7 @@ namespace Game
             ownedCardList.itemRenderer = ItemRenderer;
             ownedCardList.ItemProvider = ItemProvider;
             ownedCardList.ItemReturn = ItemReturn;
-            
+
             _valueUIItems = valueUIRoot.GetComponentsInChildren<ValueUIItem>();
         }
 
@@ -33,7 +33,7 @@ namespace Game
 
         private GameObject ItemProvider(int idx)
         {
-            GameObject obj =  fixedCardPool.Get();
+            GameObject obj = fixedCardPool.Get();
             obj.name = idx.ToString();
             return obj;
         }
@@ -57,18 +57,18 @@ namespace Game
         public void Bind(HuluData data)
         {
             _data = data;
-            
-            _valueUIItems[0].valueNum.text = data.config.BaseHp.ToString();
-            _valueUIItems[0].slider.value = (float)data.config.BaseHp / data.config.MaxHp;
-            _valueUIItems[1].valueNum.text = data.config.BaseAtk.ToString();
-            _valueUIItems[1].slider.value = (float)data.config.BaseAtk / data.config.MaxAtk;
-            _valueUIItems[2].valueNum.text = data.config.BaseDef.ToString();
-            _valueUIItems[2].slider.value = (float)data.config.BaseDef / data.config.MaxDef;
-            _valueUIItems[3].valueNum.text = data.config.BaseSpeed.ToString();
-            _valueUIItems[3].slider.value = (float)data.config.BaseSpeed / data.config.MaxSpeed;
-            _valueUIItems[4].valueNum.text = data.config.BaseAdap.ToString();
-            _valueUIItems[4].slider.value = (float)data.config.BaseAdap / data.config.MaxAdap;
-            
+
+            _valueUIItems[0].valueNum.text = data.currentHp.ToString();
+            _valueUIItems[0].slider.value = (float)data.currentHp / data.config.MaxHp;
+            _valueUIItems[1].valueNum.text = data.currentAtk.ToString();
+            _valueUIItems[1].slider.value = (float)data.currentAtk / data.config.MaxAtk;
+            _valueUIItems[2].valueNum.text = data.currentAtk.ToString();
+            _valueUIItems[2].slider.value = (float)data.currentDef / data.config.MaxDef;
+            _valueUIItems[3].valueNum.text = data.currentSpeed.ToString();
+            _valueUIItems[3].slider.value = (float)data.currentSpeed / data.config.MaxSpeed;
+            _valueUIItems[4].valueNum.text = data.currentAdap.ToString();
+            _valueUIItems[4].slider.value = (float)data.currentAdap / data.config.MaxAdap;
+
             ownedCardList.totalCount = data.ownedSkills.Count;
             ownedCardList.RefillCells();
             ownedCardList.RefreshCells();
