67b30f039036602d21f3640ebac0e4d1d25f2f7f
danhuangjiang <1027881464@qq.com>
2024-08-10 19:51:24 +0800

    feat：选人界面

--- Assets/Game/GamePlayOutside/PlayOutsideState/SelectOpponentState.cs
+++ Assets/Game/GamePlayOutside/PlayOutsideState/SelectOpponentState.cs
@@ -13,37 +13,7 @@ namespace Game
 
         public void OnEnter(GamePlayOutsideMgr owner, IStateMachine<GamePlayOutsideMgr> stateMachine)
         {
-            var opponentList = GamePlayOutsideMgr.Singleton.opponents;
-            var metList = GamePlayOutsideMgr.Singleton.dateSystem.haveMet;
-            opponentList.Shuffle();
-            List<HuluData> hulus = new List<HuluData>();
-            for (int i=0;i<opponentList.Count;++i)
-            {
-                if (!metList.Contains(opponentList[i].id))
-                {
-                    metList.Add(opponentList[i].id);
-                    opponentList[i].hulus.Shuffle();
-                    for (int j = 0; j < 3;++j)
-                    {
-                        var hulu = new HuluData();
-                        hulu.id = opponentList[i].hulus[j];
-                        hulu.Roll9Skills();
-                        hulus.Add(hulu);
-                    }
-                    break;
-                }
-            }
-            var player = Global.Get<DataSystem>().Get<GameData>().playerData.trainerData;
-            var n_player = new TrainerData();
-            n_player.trainerSkills = player.trainerSkills;
-            n_player.datas.Add(player.datas[0]);
-            n_player.datas.Add(player.datas[1]);
-            n_player.datas.Add(player.datas[2]);
-            var enemy = new TrainerData();
-            enemy.RollTrainerSkill9();
-            enemy.datas = hulus;
-            var env = GameMath.RandomBattleEnvData();
-            Global.Get<GameFlow>().ToGameBattle(n_player, enemy, env);
+            UIRoot.Singleton.OpenPanel<SelectHuluPanel>();
         }
 
         public void OnUpdate(GamePlayOutsideMgr owner, IStateMachine<GamePlayOutsideMgr> stateMachine)
@@ -52,7 +22,7 @@ namespace Game
 
         public void OnExit(GamePlayOutsideMgr owner, IStateMachine<GamePlayOutsideMgr> stateMachine)
         {
-
+            UIRoot.Singleton.ClosePanel<SelectHuluPanel>();
         }
     }
 }
