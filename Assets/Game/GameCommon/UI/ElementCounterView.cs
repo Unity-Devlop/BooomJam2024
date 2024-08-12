f314fc65bccbfaf0256db7d40a4977b2a1b7d353
NicoIer <91652330+NicoIer@users.noreply.github.com>
2024-08-13 01:44:45 +0800

    feat:更新克制表UI

--- Assets/Game/GameCommon/UI/ElementCounterView.cs
+++ Assets/Game/GameCommon/UI/ElementCounterView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using UnityToolkit;
@@ -10,10 +12,25 @@ namespace Game
     {
         [SerializeField] private Button showButton;
         [SerializeField] private RectTransform content;
-
+        [SerializeField] private RectTransform counterContainer;
+        // [SerializeField] private Image[] counterIconList;
+        // [SerializeField] private GameObject prefab;
         private void Awake()
         {
             showButton.onClick.AddListener(ShowButtonClick);
+            // counterIconList = counterContainer.GetComponentsInChildren<Image>();
+            // int elementCount = Global.Table.ElementFitTable.DataList.Count;
+            // Assert.IsTrue(counterIconList.Length == Global.Table.ElementFitTable.DataList.Count *
+            //     Global.Table.ElementFitTable.DataList.Count);
+
+            // for (int atk = 0; atk < elementCount; atk++)
+            // {
+            //     for (int def = 0; def < elementCount; def++)
+            //     {
+            //         
+            //     }
+            // }
+            
             HideContent();
         }
 
