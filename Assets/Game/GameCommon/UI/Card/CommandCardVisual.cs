8e01d7749d8a86828b9f31580d45f0391e5a42c7
NicoIer <91652330+NicoIer@users.noreply.github.com>
2024-08-15 22:06:18 +0800

    feat:指挥技能加载

--- Assets/Game/GameCommon/UI/Card/CommandCardVisual.cs
+++ Assets/Game/GameCommon/UI/Card/CommandCardVisual.cs
@@ -8,9 +8,26 @@ namespace Game
         [SerializeField] private TextMeshProUGUI descText;
         [SerializeField] private TextMeshProUGUI nameEngText;
 
+        [SerializeField] private RectTransform typeContainer;
+
         public override async void Initialize(Card card)
         {
             base.Initialize(card);
+
+            if (!string.IsNullOrEmpty(
+                    card.data.config.CardImagePath) && !string.IsNullOrWhiteSpace(card.data.config.CardImagePath))
+            {
+                background.sprite = await Global.Get<ResourceSystem>().LoadImage(card.data.config.CardImagePath);
+                typeContainer.gameObject.SetActive(false);
+            }
+            else
+            {
+                background.sprite = await Global.Get<ResourceSystem>()
+                    .LoadImage("UI/CardAtlas/command/Command_Porsche_background.png");
+                typeContainer.gameObject.SetActive(true);
+            }
+
+
             descText.text = card.data.config.Desc;
             nameEngText.text = card.data.config.EngName;
         }
